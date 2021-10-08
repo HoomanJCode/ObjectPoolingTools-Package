41100adbf41edd51e6165ae6caa5b95af813ac4a
Hooman <Hooman.Jalalpoor@gmail.com>
2021-10-08 18:19:40 +0330

    ObjectPool changed to GameObjectPool and created an other ObjectPool

--- Runtime/ObjectPool.cs
+++ Runtime/ObjectPool.cs
@@ -1,57 +1,40 @@
 ﻿using System;
 using System.Collections;
-using System.Linq;
 using UnityEngine;
 using Object = UnityEngine.Object;
 using Random = UnityEngine.Random;
 
-public class ObjectPool : ObjectPool<Transform>
+public interface ICloneable<out T>
 {
-    public ObjectPool(GameObject objectPrefab, ushort capacity = 1, int initializeFrameStep = 0) : base(
-        objectPrefab.transform, capacity, initializeFrameStep)
-    {
-    }
-
-    // ReSharper disable once ParameterTypeCanBeEnumerable.Local
-    public ObjectPool(GameObject[] objectPrefab, ushort capacity = 1, int initializeFrameStep = 0) : base(
-        objectPrefab.Select(x => x.transform).ToArray(), capacity, initializeFrameStep)
-    {
-    }
+    T Clone();
 }
 
-public class ObjectPool<TComponent> : IDisposable where TComponent : Component
+public class ObjectPool<T> : IDisposable where T : ICloneable<T>, IDisposable
 {
     private readonly int _capacity;
-    private readonly TComponent[] _objectPrefab;
+    private readonly T[] _objectPrefab;
 
     // ReSharper disable once MemberCanBePrivate.Global
     public readonly ObjectPoolBehaviour BehaviourObject;
-    public readonly TComponent[] Objects;
+    public readonly T[] Objects;
     private int _currentPosition = -1;
 
-    public ObjectPool(TComponent objectPrefab, ushort capacity = 1, int initializeFrameStep = 0)
+    public ObjectPool(T objectPrefab, ushort capacity = 1, int initializeFrameStep = 0)
     {
-        if (!objectPrefab)
-        {
-            Debug.LogError($"{nameof(objectPrefab)} Can not be null!");
-            return;
-        }
-
         if (capacity < 1)
         {
             Debug.LogError($"{nameof(capacity)} should be 1 or higher.");
             return;
         }
 
-        BehaviourObject = new GameObject($"{nameof(ObjectPool<TComponent>)} of {objectPrefab.name}")
-            .AddComponent<ObjectPoolBehaviour>();
+        BehaviourObject = new GameObject($"{nameof(ObjectPool<T>)}").AddComponent<ObjectPoolBehaviour>();
         _objectPrefab = new[] {objectPrefab};
         _capacity = capacity;
-        Objects = new TComponent[capacity];
+        Objects = new T[capacity];
         if (initializeFrameStep > 0) Initialize(initializeFrameStep);
     }
 
-    public ObjectPool(TComponent[] objectPrefab, ushort capacity = 1, int initializeFrameStep = 0)
+    public ObjectPool(T[] objectPrefab, ushort capacity = 1, int initializeFrameStep = 0)
     {
         if (objectPrefab == null || objectPrefab.Length < 1)
         {
@@ -65,14 +48,12 @@ public class ObjectPool<TComponent> : IDisposable where TComponent : Component
             return;
         }
 
-        BehaviourObject = new GameObject($"{nameof(ObjectPool<TComponent>)} of {objectPrefab[0].name},...")
-            .AddComponent<ObjectPoolBehaviour>();
+        BehaviourObject = new GameObject($"{nameof(ObjectPool<T>)} ,...").AddComponent<ObjectPoolBehaviour>();
         _objectPrefab = objectPrefab;
         _capacity = capacity;
-        Objects = new TComponent[capacity];
+        Objects = new T[capacity];
         if (initializeFrameStep > 0) Initialize(initializeFrameStep);
     }
-
 
     public void Dispose()
     {
@@ -90,90 +71,30 @@ public class ObjectPool<TComponent> : IDisposable where TComponent : Component
     {
         for (var i = 0; i < _capacity; i++)
         {
-            if (Objects[i]) Object.Destroy(Objects[i].gameObject);
             if (_objectPrefab == null || _objectPrefab.Length < 1) yield break;
             var randomPrefab = _objectPrefab[Random.Range(0, _objectPrefab.Length)];
-            if (!randomPrefab) yield break;
-            Objects[i] = Object.Instantiate(randomPrefab.gameObject, BehaviourObject.transform)
-                .GetComponent<TComponent>();
-            Objects[i].gameObject.SetActive(false);
+            if (randomPrefab == null) yield break;
+            Objects[i] = randomPrefab.Clone();
             for (var c = 0; c < frameStep; c++) yield return null;
         }
     }
 
-    private static IEnumerator DeActive(GameObject obj, float deActiveAfter)
+
+    public T GetNext()
     {
-        yield return new WaitForSeconds(deActiveAfter);
-        obj.SetActive(false);
-    }
-
-
-    public TComponent ActiveNext(Vector3 position, float deActiveAfter = 0)
-    {
-        return ActiveNext(true, position, deActiveAfter);
-    }
-
-    public TComponent ActiveNext(float deActiveAfter = 0)
-    {
-        return ActiveNext(false, default, deActiveAfter);
-    }
-
-    private TComponent ActiveNext(bool setNewPosition, Vector3 position, float deActiveAfter)
-    {
-        TComponent MakeReadyCurrentPosition(int index)
-        {
-            if (setNewPosition) Objects[index].transform.position = position;
-            Objects[index].gameObject.SetActive(true);
-            if (deActiveAfter > 0)
-                BehaviourObject.StartCoroutine(DeActive(Objects[index].gameObject, deActiveAfter));
-            return Objects[index];
-        }
-
         //check is there any inactive
-        var tempPosition = _currentPosition;
-        for (var c = 0; c < _capacity; c++)
-        {
-            tempPosition++;
-            var index = tempPosition % _capacity;
-            if (!Objects[index])
-            {
-                var randomPrefab = _objectPrefab[Random.Range(0, _objectPrefab.Length)];
-                Objects[index] = Object.Instantiate(randomPrefab.gameObject, BehaviourObject.transform)
-                    .GetComponent<TComponent>();
-                Objects[index].gameObject.SetActive(false);
-            }
-
-            if (Objects[index].gameObject.activeInHierarchy) continue;
-            _currentPosition = tempPosition;
-            return MakeReadyCurrentPosition(index);
-        }
-
-        //get next active if not found
-        for (var c = 0; c < _capacity; c++)
-        {
-            var index = ++_currentPosition % _capacity;
-            if (!Objects[index]) continue;
-            Objects[index].gameObject.SetActive(false);
-            return MakeReadyCurrentPosition(index);
-        }
-
-        return null;
-    }
-
-    public void DeActiveAll()
-    {
-        foreach (var monoBehaviour in Objects)
-            if (monoBehaviour)
-                monoBehaviour.gameObject.SetActive(false);
-        _currentPosition = -1;
+        var index = ++_currentPosition % _capacity;
+        if (Objects[index] == null)
+            Objects[index] = _objectPrefab[Random.Range(0, _objectPrefab.Length)].Clone();
+        return Objects[index];
     }
 
     private void ReleaseUnmanagedResources()
     {
         if (BehaviourObject) Object.Destroy(BehaviourObject.gameObject);
         for (var i = 0; i < _capacity; i++)
-            if (Objects[i])
-                Object.Destroy(Objects[i].gameObject);
+            if (Objects[i] != null)
+                Objects[i].Dispose();
     }
 
     ~ObjectPool()
